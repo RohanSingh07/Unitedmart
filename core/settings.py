b5943e93495fcf984da8982b7407ad4d30163f58
Rohan Singh <singhrohanss007@gmail.com>
2022-04-08 14:00:05 +0530

    08/04/2022

--- core/settings.py
+++ core/settings.py
@@ -125,12 +125,11 @@ DATABASES = {
 ELASTICSEARCH_DSL ={
     'default':{
                           #name : # password             @ #Endpoint
-        'hosts':'https://elastic:KMRymlglTBSTFI9PGckZFnzH@elastic-d09a59.es.us-central1.gcp.cloud.es.io:9243/',
-
+        'hosts':'https://elastic:DtAD3xJxxyw60M3VzEhCnZOo@elastic.es.us-central1.gcp.cloud.es.io:9243/',
         'timeout': 60,
 
         #'hosts':'localhost',
-        # rohan10igi@gmail.com
+        # rohan12igi@gmail.com
     }
 
 }
